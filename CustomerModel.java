3e50eab014bc7040432bbc1d7369ceae8998b898
Rerutnev <141374762+Rerutnev@users.noreply.github.com>
2023-08-03 21:14:01 -0400

    Update CustomerModel.java

--- CustomerModel.java
+++ CustomerModel.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList; 
 
 
-
 public class CustomerModel {
 	public customerModel customer;
-  public string username;
-  public string email;
-  public string password;
-  public string address;
+  public String username;
+  public String email;
+  public String password;
+  public String address;
   public int userId;
   public BasketModel customerBasket;
   public int numberOfCoupons;
@@ -21,80 +20,122 @@ public class CustomerModel {
     userId = -1;
     customerBasket = new BasketModel();
     numberOfCoupons = 0;
+	}
 
+	void setCustomer(customerModel customer) {
+		this.customer = customer; 
 	}
 	
-  void updateUserDetails() {
-		items.add(item); 
-		count.add(1); 
-		double price = item.getPrice(); 
-		subtotal += price; 
-		tax = (0.15 * subtotal); 
-		total = subtotal + tax; 
-		System.out.println(item.getName() + " added to basket.");
-		//Adds a product to the Basket and updates the Subtotal, Tax, and Total attributes.  
+	boolean setUsername(String username) {
+		this.username = username;
+	}
+	
+	void setEmail(String email) {
+		this.email = email; 
+	}
+	
+	void setPassword (String password) {
+		this.password = password; 
+	}
+	
+	void setAddress(String address) {
+		this.address = address; 
+	}
+	
+	void setUserId(int userId) {
+		this.userId = userId; 
+	}
+
+	void setCustomerBasket(BasketModel customerBasket) {
+		this.customerBasket = customerBasket; 
+	}
+	
+	void setNnumberOfCoupons(int numberOfCoupons) {
+		this.numberOfCoupons = numberOfCoupons; 
+	}
+	
+	customerModel getCustomer() {
+		return customer; 
+	}
+		
+	String getUsername() {
+		return username; 
+	}
+	
+	String getEmail() {
+		return setEmail; 
+	}
+	
+	String getPassword() {
+		return password; 
+	}
+	
+	String getAddress() {
+		return address; 
+	}
+	
+	String getUserId() {
+		return userId; 
+	}
+	
+	String getCustomerBasket() {
+		return customerBasket; 
+	}
+	
+	String getNnumberOfCoupons() {
+		return numberOfCoupons; 
+	}
+
+  void updateUserDetails(String username, String email, String password, String address, int userID, BasketModel customerBasket, int numberOfCoupons) {
+		this.username = username;
+    this.email = email;
+    this.password = password;
+    this.address = address;
+    this.userId = userId;
+    this.customerBasket = customerBasket;
+    this.numberOfCoupons = numberOfCoupons;
+		System.out.println(username + " updated as a user.");
+		// Updates the user information in the SQL database by changing an existing entry
 	}
   
 	int useCoupon(int numberOfCoupons) {
-		int i = 0; 
-		for (ProductModel item : items) {
-			if (item.id == product.id) {
-				double price = item.getPrice() * count.get(i);
-				items.remove(i);
-				count.remove(i); 
-				subtotal -= price; 
-				tax = (0.15 * subtotal); 
-				total = subtotal + tax; 
-				return true; 
-			}
-			i++;
+		int i = getNumberOfCoupons();
+		if (i > numberOfCoupons) {
+			return numberOfCoupons; 
+		} else {
+			System.out.println("Number of available coupons has been excided! No coupons were used.");
+			return 0;
 		}
-		return false; 
 	}
   
 	int addCoupon(int priceOfOrder) {
-		int i = 0; 
-		for (ProductModel item : items) {
-			if (item.id == product.id) {
-				double price = item.getPrice() * quantity; 
-				count.set(i, quantity);  
-				subtotal += price; 
-				tax = (0.15 * subtotal); 
-				total = subtotal + tax; 
-				return; 
-			}
-			i++;
+		int i = priceOfOrder/100;
+		if (i > 0) {
+			setNumberOfCoupons = getNumberOfCoupons + i;
+			System.out.println( i + " coupon(s) have been added.");
+			return i;
 		}
 	}
   
 	int addtem(int productID) {
-		items.clear();
-		count.clear();
-		subtotal = 0; 
-		tax = 0; 
-		total = 0; 
+		BasketModel.addProduct(productID);
+		return 1;
 	}
   
 	int removetem(ind productID) {
-		if (items.isEmpty()) {
-			return true;
-		}
-		return false; 
+		BasketModel.removeProduct(productID);
+		return 1; 
 	}
 
 	@Override
     public String toString() {
-		String result = "Items in basket : \n"; 
+		String result = "Customers in the database : \n"; 
 		int i = 0; 
-		for (ProductModel item : items) {
-			result += item.getName() + ", " + count.get(i) + "\n"; 
+		for (CustomerModel customer : customers) {
+			result += item.getUsername() + ", " + count.get(i) + "\n"; 
 			i++; 
 		}
-		result += "Subtotal : " + (Math.round(subtotal * 1000)/1000.00) + "\n"; 
-		result += "Taxes : " + (Math.round(tax * 1000)/1000.00) + "\n";
-		result += "Total : " + (Math.round(total * 100)/100.00) + "\n"; 
-		
-        return result; 
+    return result; 
     }
 
 }
